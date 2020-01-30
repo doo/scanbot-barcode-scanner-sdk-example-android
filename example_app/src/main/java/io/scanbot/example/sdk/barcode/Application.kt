b819d2bb6715f23d60a8670647cdaad163c40121
Eduard Frank <eddy@scanbot.io>
2020-01-30 12:53:51 +0100

    Update to latest snapshot "0.0.1.1-SNAPSHOT". Small improvements, fixes, wording, etc.

--- example_app/src/main/java/io/scanbot/example/sdk/barcode/Application.kt
+++ example_app/src/main/java/io/scanbot/example/sdk/barcode/Application.kt
@@ -3,45 +3,44 @@ package io.scanbot.example.sdk.barcode
 import android.app.Application
 import io.scanbot.sap.IScanbotSDKLicenseErrorHandler
 import io.scanbot.sap.SdkFeature
+import io.scanbot.sdk.barcode_scanner.ScanbotBarcodeScannerSDK
 import io.scanbot.sdk.barcode_scanner.ScanbotBarcodeScannerSDKInitializer
 import net.doo.snap.util.log.LoggerProvider
 
-class SnapApplication : Application() {
-    val licenseKey =
-        "BSMZQgVt1mQTbh8hCdeADm4BrX1Exm" +
-                "4OiTLy9Itr7vCvosA9HQ+LBCwlvJdB" +
-                "gH8NvR7jXrop4+o1jD2hYX1UYdxcrF" +
-                "owZuqzbYz7a19nKZKrKZ9M4KRKoWnk" +
-                "hSrrG/xqa6n1YhndjmmArgHrhKahYR" +
-                "QL1wVeP4xNttYQaDqRsbn5nvlkDKbo" +
-                "BlEuQiab5l/7hqcyCCZIk3HnYPWCtT" +
-                "/+iB/Lw5fxBJ9YxxHAlz598myK99t4" +
-                "RFF4IPU09WTeFgoyUr6AbyHqSMIPDR" +
-                "OdlJxWMvtO4xV3T5M4hX5XMFLf+FYP" +
-                "CEiVwbOnwctTr5zKEVAaH85fhYqBdb" +
-                "MSs/BVCE1Dug==\nU2NhbmJvdFNESw" +
-                "ppby5zY2FuYm90LmV4YW1wbGUuc2Rr" +
-                "LmJhcmNvZGUuYW5kcm9pZAoxNTgyMz" +
-                "I5NTk5CjUxMgoy\n";
+class ExampleApplication : Application() {
+    /*
+     * TODO 1/2: Add the Scanbot Barcode SDK license key here.
+     * Please note: The Scanbot Barcode SDK will run without a license key for one minute per session!
+     * After the trial period is over all Scanbot SDK functions as well as the UI components will stop working.
+     * You can get an unrestricted "no-strings-attached" 30 day trial license key for free.
+     * Please submit the trial license form (https://scanbot.io/sdk/trial.html) on our website by using
+     * the app identifier "io.scanbot.example.sdk.barcode.android" of this example app.
+     */
+    val licenseKey = ""
 
     override fun onCreate() {
         super.onCreate()
 
         ScanbotBarcodeScannerSDKInitializer()
             .withLogging(true)
-            .license(this, licenseKey)
+            // TODO 2/2: Enable the Scanbot Barcode SDK license key
+            //.license(this, licenseKey)
             .licenceErrorHandler(IScanbotSDKLicenseErrorHandler { status, feature ->
-                LoggerProvider.getLogger()
-                    .d("ScanbotBarcodeScannerSDK", "license status:${status.name}")
+                LoggerProvider.getLogger().d("ExampleApplication", "+++> License status: ${status.name}")
                 if (feature != SdkFeature.NoSdkFeature) {
-                    LoggerProvider.getLogger()
-                        .d("ScanbotBarcodeScannerSDK", "feature not available:${feature.name}")
+                    LoggerProvider.getLogger().d("ExampleApplication", "+++> Feature not available: ${feature.name}")
                 }
             })
-            .sdkFilesDirectory(this, getExternalFilesDir(null)!!)
+            //.sdkFilesDirectory(this, getExternalFilesDir(null)!!)
             .prepareBarcodeScannerBlobs(true)
             .initialize(this)
 
+        LoggerProvider.getLogger().d("ExampleApplication", "Scanbot Barcode Scanner SDK was initialized")
+
+        val licenseInfo = ScanbotBarcodeScannerSDK(this).licenseInfo
+        LoggerProvider.getLogger().d("ExampleApplication", "License status: ${licenseInfo.status}")
+        LoggerProvider.getLogger().d("ExampleApplication", "License isValid: ${licenseInfo.isValid}")
+        LoggerProvider.getLogger().d("ExampleApplication", "License expirationDate: ${licenseInfo.expirationDate}")
     }
 }
 
