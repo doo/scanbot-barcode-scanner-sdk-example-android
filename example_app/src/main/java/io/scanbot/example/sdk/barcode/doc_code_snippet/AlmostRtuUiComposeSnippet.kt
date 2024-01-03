294156df4b83c3e3ed4af4b59924323b9afdeda9
Den Drobiazko <denys@scanbot.io>
2024-01-03 13:12:10 +0100

    Bump up SDK version, implement snippets

---
+++ example_app/src/main/java/io/scanbot/example/sdk/barcode/doc_code_snippet/AlmostRtuUiComposeSnippet.kt
@@ -0,0 +1,79 @@
+package io.scanbot.example.sdk.barcode.doc_code_snippet
+
+/*
+    NOTE: this snippet of code is to be used only as a part of the website documentation.
+    This code is not intended for any use outside of the support of documentation by Scanbot SDK GmbH employees.
+*/
+
+// NOTE for maintainers: whenever changing this code,
+// ensure that links using it are still pointing to valid lines!
+// Pay attention to imports adding/removal/sorting!
+// Page URLs using this code:
+// TODO: add URLs here
+
+import android.os.Build
+import android.os.Bundle
+import android.view.WindowManager
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
+import androidx.compose.ui.platform.ComposeView
+import androidx.core.view.WindowCompat
+import io.scanbot.example.sdk.barcode.R
+import io.scanbot.sdk.ui_v2.barcode.BarcodeScannerView
+import io.scanbot.sdk.ui_v2.barcode.configuration.BarcodeScannerConfiguration
+import io.scanbot.sdk.ui_v2.common.StatusBarMode
+import io.scanbot.sdk.ui_v2.common.activity.AutoCancelTimeout
+import io.scanbot.sdk.ui_v2.common.activity.CanceledByUser
+import io.scanbot.sdk.ui_v2.common.activity.LicenseInvalid
+
+class AlmostRtuUiBarcodeScannerActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        supportRequestWindowFeature(WindowCompat.FEATURE_ACTION_BAR_OVERLAY)
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.doc_snippet_activity_artu_barcode_scanner)
+
+        val cameraContainerView: ComposeView = findViewById(R.id.compose_container)
+        cameraContainerView.apply {
+            setContent {
+                val configuration = remember {
+                    BarcodeScannerConfiguration().apply {
+                        // TODO: configure as needed
+                    }
+                }
+
+                val statusBarHidden = configuration.topBar.statusBarMode == StatusBarMode.HIDDEN
+                LaunchedEffect(key1 = true, block = {
+                    if (statusBarHidden) {
+                        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+                            window.attributes.layoutInDisplayCutoutMode = WindowManager.LayoutParams.LAYOUT_IN_DISPLAY_CUTOUT_MODE_SHORT_EDGES
+                        }
+
+                        WindowCompat.setDecorFitsSystemWindows(window, false)
+                    }
+                })
+
+                BarcodeScannerView(
+                    configuration = configuration,
+                    onBarcodeScanned = {
+                        // TODO: present barcode result as needed.
+                    },
+                    onBarcodeScannerClosed = {
+                        when (it) {
+                            LicenseInvalid -> Toast.makeText(
+                                context,
+                                "License has expired!",
+                                Toast.LENGTH_LONG
+                            ).show()
+                            AutoCancelTimeout -> Unit // just close screen (below)
+                            CanceledByUser -> Unit // just close screen (below)
+                        }
+                        finish()
+                    }
+                )
+            }
+        }
+    }
+}
