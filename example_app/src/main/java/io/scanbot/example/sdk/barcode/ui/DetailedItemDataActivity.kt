6c1b11f3a9477498ebaa449138ee9f690770cc2b
Artem Kubiria <artem@scanbot.io>
2024-05-28 16:58:37 +0200

    Refactored Barcode Parsed Documents representation

--- example_app/src/main/java/io/scanbot/example/sdk/barcode/ui/DetailedItemDataActivity.kt
+++ example_app/src/main/java/io/scanbot/example/sdk/barcode/ui/DetailedItemDataActivity.kt
@@ -3,6 +3,7 @@ package io.scanbot.example.sdk.barcode.ui
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import io.scanbot.barcodescanner.entity.AAMVA
+import io.scanbot.barcodescanner.entity.BarcodeDocumentLibrary.wrap
 import io.scanbot.barcodescanner.entity.BoardingPass
 import io.scanbot.barcodescanner.entity.DEMedicalPlan
 import io.scanbot.barcodescanner.entity.GS1
@@ -30,87 +31,87 @@ class DetailedItemDataActivity : AppCompatActivity() {
     }
 
     private fun printParsedFormat(item: BarcodeItem): String {
-        val formattedResult = item.parsedDocument
+        val formattedResult = item.parsedDocument?.wrap()
                 ?: return "${item.textWithExtension}\n\nBinary data:\n${item.rawBytes.toHexString()}" // for not supported by current barcode detector implementation
 
         val barcodesResult = StringBuilder()
-        when (formattedResult.type.name) {
-            BoardingPass.DOCUMENT_TYPE -> {
+        when (formattedResult) {
+            is BoardingPass -> {
                 barcodesResult.append("\n")
                     .append("Boarding Pass Document\n")
-                formattedResult.fields.forEach { field ->
+                formattedResult.document.fields.forEach { field ->
                     barcodesResult.append("${field.type.name}:${field.value?.text}\n")
                 }
-                formattedResult.children.forEach { leg ->
+                formattedResult.document.children.forEach { leg ->
                     leg.fields.forEach {
                         barcodesResult.append("${it.type.name}:${it.value?.text}\n")
                     }
                 }
             }
-            SwissQR.DOCUMENT_TYPE -> {
+            is SwissQR -> {
                 barcodesResult.append("\nSwiss QR Document\n")
 
-                formattedResult.fields.forEach {
+                formattedResult.document.fields.forEach {
                     barcodesResult.append("${it.type.name}: ${it.value?.text}\n")
                 }
             }
-            DEMedicalPlan.DOCUMENT_TYPE -> {
+            is DEMedicalPlan -> {
                 barcodesResult.append("\nDE Medical Plan Document\n")
 
-                formattedResult.fields.forEach { field ->
+                formattedResult.document.fields.forEach { field ->
                     barcodesResult.append(field.type.name).append(": ").append(field.value?.text)
                         .append("\n")
                 }
-                formattedResult.children.forEach { subfile ->
+                formattedResult.document.children.forEach { subfile ->
                     for (field in subfile.fields) {
                         barcodesResult.append(field.type.name).append(": ").append(field.value?.text)
                             .append("\n")
                     }
                 }
             }
-            IDCardPDF417.DOCUMENT_TYPE -> {
+            is IDCardPDF417 -> {
                 barcodesResult.append("\nId Card PDF417\n")
 
-                formattedResult.fields.forEach {
+                formattedResult.document.fields.forEach {
                     barcodesResult.append("${it.type?.name}: ${it.value?.text}\n")
                 }
             }
-            GS1.DOCUMENT_TYPE -> {
+            is GS1 -> {
                 barcodesResult.append("\nGs1 Document\n")
 
-                formattedResult.fields.forEach {
+                formattedResult.document.fields.forEach {
                     barcodesResult.append("${it.type.name}: ${it.value?.text}\n")
                 }
             }
-            SEPA.DOCUMENT_TYPE -> {
+            is SEPA -> {
                 barcodesResult.append("\nSEPA Document\n")
 
-                formattedResult.fields.forEach {
+                formattedResult.document.fields.forEach {
                     barcodesResult.append("${it.type.name}: ${it.value?.text}\n")
                 }
             }
-            MedicalCertificate.DOCUMENT_TYPE -> {
+            is MedicalCertificate -> {
                 barcodesResult.append("\nMedical Certificate Document\n")
 
-                formattedResult.fields.forEach {
+                formattedResult.document.fields.forEach {
                     barcodesResult.append("${it.type?.name}: ${it.value?.text}\n")
                 }
             }
-            VCard.DOCUMENT_TYPE -> {
+            is VCard -> {
                 barcodesResult.append("\nVcard Document\n")
 
-                formattedResult.fields.forEach {
+                formattedResult.document.fields.forEach {
                     barcodesResult.append("${it.type.name}: ${it.value?.text}\n")
                 }
             }
-            AAMVA.DOCUMENT_TYPE -> {
+            is AAMVA -> {
                 barcodesResult.append("\n")
                     .append("AAMVA Document\n")
-                formattedResult.fields.forEach { field ->
+                formattedResult.document.fields.forEach { field ->
                     barcodesResult.append(field.type.name).append(": ").append(field.value?.text)
                         .append("\n")
                 }
-                formattedResult.children.forEach { subfile ->
+                formattedResult.document.children.forEach { subfile ->
                     for (field in subfile.fields) {
                         barcodesResult.append(field.type.name).append(": ").append(field.value?.text)
                             .append("\n")
