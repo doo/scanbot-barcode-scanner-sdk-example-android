c2a87ee89f8cdbf2d06dcef7011e06e55c2d1f09
Serhii Chaban <serhii@scanbot.io>
2024-10-30 15:43:02 +0100

    update android BS 6.1.0 qa branch

---
+++ settings.gradle.kts
@@ -0,0 +1,21 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+        // Scanbot SDK maven repos:
+        maven(url = "https://nexus.scanbot.io/nexus/content/repositories/releases/")
+        maven(url = "https://nexus.scanbot.io/nexus/content/repositories/snapshots/")
+    }
+}
+
+include(":example_app")
+rootProject.name = "Scanbot Barcode Scanner SDK example"
